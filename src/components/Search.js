d4af5d12c724025bf747bfdcf8df5ec163967ccb
Jay Moon <sjaymoon15@gmail.com>
2021-01-20 16:32:56 -0600

    Use useState in Search and update Weather to be a function instead of a component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,44 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Form } from 'semantic-ui-react';
 import searchStyles from '../styles/searchStyles.module.css';
 
-export default class Search extends Component {
-  constructor(props) {
-    super(props);
+const Search = ({ fetchWeathers }) => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-    this.state = {
-      searchQuery: '',
-    };
-  }
-
-  handleInputChange = (e) => {
-    this.setState({ searchQuery: e.target.value });
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
   };
 
-  handleFormSubmit = () => {
-    this.props.fetchWeathers(this.state.searchQuery);
-    this.setState({ searchQuery: '' });
+  const handleFormSubmit = () => {
+    fetchWeathers(searchQuery);
+    setSearchQuery('');
   };
 
-  render() {
-    return (
-      <div
-        data-test='component-search'
-        className={searchStyles.searchContainer}
-      >
-        <Form onSubmit={this.handleFormSubmit}>
-          <Form.Input
-            fluid
-            action={{
-              color: 'teal',
-              content: 'Search',
-            }}
-            placeholder='Enter City or Zipcode'
-            value={this.state.searchQuery}
-            onChange={this.handleInputChange}
-          />
-        </Form>
-      </div>
-    );
-  }
-}
+  return (
+    <div data-test='component-search' className={searchStyles.searchContainer}>
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Input
+          fluid
+          action={{
+            color: 'teal',
+            content: 'Search',
+          }}
+          placeholder='Enter City or Zipcode'
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+      </Form>
+    </div>
+  );
+};
+
+export default Search;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   ResponsiveContainer,
   LineChart,
@@ -16,38 +16,33 @@ import {
   LINE_COLOR_TEMP,
 } from '../utils/constants';
 
-export default class Weather extends Component {
-  render() {
-    const { list } = this.props;
-    return (
-      <ResponsiveContainer
-        width='100%'
-        aspect={2}
-        data-test='component-weather'
-      >
-        <LineChart data={list} margin={{ bottom: 5 }}>
-          <CartesianGrid strokeDasharray='3 3' />
-          <XAxis dataKey='dt_txt' />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            dataKey='main.pressure'
-            stroke={LINE_COLOR_PRESSURE}
-            name='Pressure (hPa)'
-          />
-          <Line
-            dataKey='main.humidity'
-            stroke={LINE_COLOR_HUMIDITY}
-            name='Humidity (%)'
-          />
-          <Line
-            dataKey='main.temp'
-            stroke={LINE_COLOR_TEMP}
-            name='Temperature (F)'
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+const Weather = ({ list }) => {
+  return (
+    <ResponsiveContainer width='100%' aspect={2} data-test='component-weather'>
+      <LineChart data={list} margin={{ bottom: 5 }}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='dt_txt' />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          dataKey='main.pressure'
+          stroke={LINE_COLOR_PRESSURE}
+          name='Pressure (hPa)'
+        />
+        <Line
+          dataKey='main.humidity'
+          stroke={LINE_COLOR_HUMIDITY}
+          name='Humidity (%)'
+        />
+        <Line
+          dataKey='main.temp'
+          stroke={LINE_COLOR_TEMP}
+          name='Temperature (F)'
+        />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Weather;
